0f173a99652050e40f1d3fc5fac43a98a32cb65a
matheusqueiroz92 <matheus_giga@hotmail.com>
2025-05-07 17:02:28 -0300

    again

--- apps/backend/src/interfaces/repositories/IBaseRepository.ts
+++
@@ -1,9 +0,0 @@
-import { ClientSession } from 'mongoose';
-
-export interface IBaseRepository<T, ID> {
-  create(entity: Partial<T>, options?: { session?: ClientSession }): Promise<T>;
-  findById(id: ID, options?: { [key: string]: any }): Promise<T | null>;
-  findAll(options?: { [key: string]: any }): Promise<T[]>;
-  update(id: ID, entity: Partial<T>, options?: { [key: string]: any }): Promise<T | null>;
-  delete(id: ID, options?: { [key: string]: any }): Promise<boolean>;
-}
